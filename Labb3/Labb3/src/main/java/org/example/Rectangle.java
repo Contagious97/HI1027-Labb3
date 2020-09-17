0d67139c34b67eb42d385b76078a3fe242b06be6
Contagious97 <62553422+Contagious97@users.noreply.github.com>
2020-09-17 17:08:08 +0200

    Added the different shapes

---
+++ Labb3/Labb3/src/main/java/org/example/Rectangle.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class Rectangle extends Shape {
+
+    private double height, width;
+
+    public Rectangle(){
+
+    }
+
+    public void paint(GraphicsContext gc){
+
+    }
+
+}
